37c2fd2e6932764333e5b1c8ed53031abe8d4cf1
dipaliwagh22 <deepaliw121@gmail.com>
2024-01-10 19:55:24 +0530

    js file changed

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-const quizQuestions = [
-    {
-        question: "Which language runs in a web browser?",
-        opt1: "Java",
-        opt2: "C",
-        opt3: "Python",
-        opt4: "JavaScript",
-        correct: "opt4",
-    },
-    {
-        question: "What does CSS stand for?",
-        opt1: "Central Style Sheets",
-        opt2: "Cascading Style Sheets",
-        opt3: "Cascading Simple Sheets",
-        opt4: "Cars SUVs Sailboats",
-        correct: "opt2",
-    },
-    {
-        question: "What does HTML stand for?",
-        opt1: "Hypertext Markup Language",
-        opt2: "Hypertext Markdown Language",
-        opt3: "Hyperloop Machine Language",
-        opt4: "Helicopters Terminals Motorboats Lamborginis",
-        correct: "opt1",
-    },
-    {
-        question: "What year was JavaScript launched?",
-        opt1: "1996",
-        opt2: "1995",
-        opt3: "1994",
-        opt4: "none of the above",
-        correct: "opt2",
-    },
-];
 let question_number_element = document.getElementById("question-number");
 let question_txt_element = document.getElementById("question-txt");
 let option_1_element = document.getElementById("option1");
@@ -40,7 +6,6 @@ let option_3_element = document.getElementById("option3");
 let option_4_element = document.getElementById("option4");
 let next_button = document.getElementById("next-button");
 let time_element = document.getElementById("timer");
-
 
 let current_question_number=0;
 let score=0;
@@ -54,9 +19,22 @@ function timer(){
     if(sec==0){
         sec = total_time;
         clearInterval(time);
+        checkIfScore();
         current_question_number++;
         showQuestion();
     }    
+}
+
+function checkIfScore(){
+    let optionIdSelected = document.querySelector('input[name = opt]:checked');
+            
+    let option_correct = quizQuestions[current_question_number].correct;
+    if(optionIdSelected!=null)
+    {            
+        if(optionIdSelected.id==option_correct){
+            score++;        
+        }
+    }
 }
 
 /**
@@ -64,7 +42,7 @@ function timer(){
  */
 
 function showQuestion(){
-    sec = total_time; //assign sec to total time of timer
+    sec = total_time; 
     clearInterval(time);
     timer();
     time = setInterval(timer,1000);
@@ -89,26 +67,18 @@ function showQuestion(){
  * Handling Event listner on button click
  */
 next_button.addEventListener('click',()=>{
-    let optionIdSelected = document.querySelector('input[name = opt]:checked');
-            
-    let option_correct = quizQuestions[current_question_number].correct;
-    if(optionIdSelected!=null)
-    {            
-        if(optionIdSelected.id==option_correct){
-            score++;        
-        }
-    }
+   checkIfScore();
     current_question_number ++;
     if(current_question_number>=quizQuestions.length){
         // show final answer
         goToResultPage();        
 
     }else{
+        //show next question
         showQuestion();
     }
-    
+        
 });
-showQuestion();
 function goToResultPage(){
     current_question_number = 0;
     localStorage.setItem("score", score);
